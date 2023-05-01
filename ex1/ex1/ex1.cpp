4541090c96eafcf9c259361b1e4ba8484a06f1cc
sheep1221 <kmkevin332907@gmail.com>
2023-05-02 01:23:11 +0800

    hw_ch2&3_20230502

--- ex1/ex1/ex1.cpp
+++ ex1/ex1/ex1.cpp
@@ -1,6 +1,4 @@
-﻿// ex1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-//#define UNICODE
+﻿//#define _UNICODE
 #include <iostream>
 #include <tchar.h>
 #include <stdlib.h>
@@ -10,19 +8,22 @@
 
 int _tmain()
 {
-    int res_t = _tcscmp(_T("Ｈｅｌｌｏ　ｗｏｒｌｄ"), _T("Ｈｅｏｌｌ　ｗｏｒｌｄ"));
-    int res_l = lstrcmp(_T("Ｈｅｌｌｏ　ｗｏｒｌｄ"), _T("Ｈｅｏｌｌ　ｗｏｒｌｄ"));
+	// for result 1
+	
+    int res_t = _tcscmp(_T("Hello world"), _T("Ｈｅｌｌｏ　ｗｏｒｌｄ"));
+    int res_l = lstrcmp(_T("Hello world"), _T("Ｈｅｌｌｏ　ｗｏｒｌｄ"));
+	int res_t2 = _tcscmp(L"Hello world", L"Ｈｅｌｌｏ　ｗｏｒｌｄ");
+	int res_l2 = lstrcmp(L"Hello world", L"Ｈｅｌｌｏ　ｗｏｒｌｄ");
+	
+	//for result 2
+	/*
+	int res_t = _tcscmp(_T("Hfllo world"), _T("Ｈｅｌｌｏ　ｗｏｒｌｄ"));
+	int res_l = lstrcmp(_T("Hfllo world"), _T("Ｈｅｌｌｏ　ｗｏｒｌｄ"));
+	int res_t2 = _tcscmp(L"Hfllo world", L"Ｈｅｌｌｏ　ｗｏｒｌｄ");
+	int res_l2 = lstrcmp(L"Hfllo world", L"Ｈｅｌｌｏ　ｗｏｒｌｄ");
+	*/
     std::cout << "res_t: " << res_t << "\n";
     std::cout << "res_l: " << res_l << "\n";
+	std::cout << "res_t2: " << res_t2 << "\n";
+	std::cout << "res_l2: " << res_l2 << "\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
